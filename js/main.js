cdf5c96264c96d3c962eb875c9c52fd42a7a30f5
IvanYank <61747305+IvanYank@users.noreply.github.com>
2021-11-21 06:39:00 +0400

    Final version

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var contact = document.querySelector(".header__contact");
+var popup = document.querySelector(".form__container");
+var close = document.querySelector(".form__close");
+var products = document.querySelectorAll(".products__item");
+var titles = document.querySelectorAll(".products__title");
+
+contact.addEventListener("click", function () {
+	popup.classList.remove("form__container_close");
+	document.querySelector("#contact_name").focus();
+});
+
+close.addEventListener("click", function () {
+	popup.classList.add("form__container_close");
+	contact.focus();
+});
