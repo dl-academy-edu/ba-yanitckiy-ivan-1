091698d863bc8f47ff007bb2fee98961807e1632
IvanYank <61747305+IvanYank@users.noreply.github.com>
2021-11-22 15:56:33 +0400

    Fix problems

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 var contact = document.querySelector(".slider__button");
 var popup = document.querySelector(".form__container");
 var close = document.querySelector(".form__close");
-var products = document.querySelectorAll(".products__item");
-var titles = document.querySelectorAll(".products__title");
+var opened = false;
 
 contact.addEventListener("click", function () {
 	popup.classList.remove("form__container_close");
 	document.querySelector("#contact_name").focus();
+	opened = true;
 });
 
 close.addEventListener("click", function () {
 	popup.classList.add("form__container_close");
 	contact.focus();
+	opened = false;
+});
+
+window.addEventListener("keydown", function (e) {
+	if (opened && e.code == "Escape") {
+		popup.classList.add("form__container_close");
+		contact.focus();
+		opened = false;
+	}
 });
